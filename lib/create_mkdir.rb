9038a620c97925eef2ff9181a6c0cc6128404678
Thom Tiber <contact@thomdeveloppeur.com>
2021-04-29 10:39:16 +0200

    Update create_mkdir.rb

--- lib/create_mkdir.rb
+++ lib/create_mkdir.rb
@@ -1,14 +1,12 @@
-# VERIFICATION de la saisie
+
 def check_if_user_gave_input
     abort("mkdir: missing input") if ARGV.empty?
 end
 
-# RECUPERATION du NOM du dossier à créer
 def get_folder_name
     return folder_name = ARGV.first
 end
 
-# CREATION du dossier (nom du dossier récupéré)
 def create_folder(name)
     check_if_user_gave_input
     Dir.mkdir(name)
@@ -20,7 +18,3 @@ def perform
 end
 
 perform
-
-# saisir => ruby create_dir.rb mon_nom_de_dossier
-# PETIT BONUS : si tu mets ce programme dans un dossier bien défini et que tu branches un alias du genre :
-# alias mkdiruby="ruby /home/ton/chemin/vers/dossier/mkdiruby.rb"
